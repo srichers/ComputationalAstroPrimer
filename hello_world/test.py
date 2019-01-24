9b88b254bb6a9d70b3946a9fb313af903da76ec0
Sherwood Richers <srichers@caltech.edu>
2019-01-23 19:07:35 -0500

    reverted python commands to python3.4

--- hello_world/test.py
+++ hello_world/test.py
@@ -2,11 +2,11 @@ import os
 import subprocess
 
 # run the program and capture the output
-result = subprocess.run("./a.out", stdout=subprocess.PIPE)
+result = subprocess.check_output("./a.out")
 
 # test that the output is what we want.
 # travis-ci will capture raised exceptions
-if(result.stdout == b"Hello World!\n"):
+if(result == b"Hello World!\n"):
     print("SUCCESS")
 else:
     raise Exception("FAILURE")
