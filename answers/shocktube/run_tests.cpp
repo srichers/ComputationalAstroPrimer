8cff3cd824c46ad8e513f0b2ebf5761ecb41ba87
Sherwood Richers <srichers@caltech.edu>
2019-03-27 14:54:45 -0400

    fix typo

--- answers/shocktube/run_tests.cpp
+++ answers/shocktube/run_tests.cpp
@@ -33,9 +33,9 @@ int main(){
   array<double,nx> rho, Eint, px;
   set_initial_conditions<nx,nghost>(3,3,rho, 3,3,Eint, 3,3,px);
   for(int i=nghost; i<nx-2*nghost; i++){
-    passing = passing & (rho [i]==3);
-    passing = passing & (Eint[i]==3);
-    passing = passing & (px  [i]==3);
+    passing = passing && (rho [i]==3);
+    passing = passing && (Eint[i]==3);
+    passing = passing && (px  [i]==3);
   }
   print_success(passing);
   if(!passing){
@@ -51,16 +51,16 @@ int main(){
   passing = true;
   for(int i=0; i<nx; i++) rho[i] = i;
   reflecting_boundary_conditions<double,nx,nghost>(rho);
-  passing = passing & (rho[0] == 5);
-  passing = passing & (rho[1] == 4);
-  passing = passing & (rho[2] == 3);
-  passing = passing & (rho[3] == 3);
-  passing = passing & (rho[4] == 4);
-  passing = passing & (rho[5] == 5);
-  passing = passing & (rho[6] == 6);
-  passing = passing & (rho[7] == 6);
-  passing = passing & (rho[8] == 5);
-  passing = passing & (rho[9] == 4);
+  passing = passing && (rho[0] == 5);
+  passing = passing && (rho[1] == 4);
+  passing = passing && (rho[2] == 3);
+  passing = passing && (rho[3] == 3);
+  passing = passing && (rho[4] == 4);
+  passing = passing && (rho[5] == 5);
+  passing = passing && (rho[6] == 6);
+  passing = passing && (rho[7] == 6);
+  passing = passing && (rho[8] == 5);
+  passing = passing && (rho[9] == 4);
   print_success(passing);
   if(!passing){
     print_array<nx,double>(rho);
@@ -73,15 +73,15 @@ int main(){
   passing = true;
   for(int i=0; i<nx; i++) rho[i] = i;
   array< array<double,nx-1>, 2> rhoLR = piecewise_constant_reconstruct<double,nx>(rho);
-  passing = passing & (rhoLR[0][0] == 0) & (rhoLR[1][0] == 1);
-  passing = passing & (rhoLR[0][1] == 1) & (rhoLR[1][1] == 2);
-  passing = passing & (rhoLR[0][2] == 2) & (rhoLR[1][2] == 3);
-  passing = passing & (rhoLR[0][3] == 3) & (rhoLR[1][3] == 4);
-  passing = passing & (rhoLR[0][4] == 4) & (rhoLR[1][4] == 5);
-  passing = passing & (rhoLR[0][5] == 5) & (rhoLR[1][5] == 6);
-  passing = passing & (rhoLR[0][6] == 6) & (rhoLR[1][6] == 7);
-  passing = passing & (rhoLR[0][7] == 7) & (rhoLR[1][7] == 8);
-  passing = passing & (rhoLR[0][8] == 8) & (rhoLR[1][8] == 9);
+  passing = passing && (rhoLR[0][0] == 0) && (rhoLR[1][0] == 1);
+  passing = passing && (rhoLR[0][1] == 1) && (rhoLR[1][1] == 2);
+  passing = passing && (rhoLR[0][2] == 2) && (rhoLR[1][2] == 3);
+  passing = passing && (rhoLR[0][3] == 3) && (rhoLR[1][3] == 4);
+  passing = passing && (rhoLR[0][4] == 4) && (rhoLR[1][4] == 5);
+  passing = passing && (rhoLR[0][5] == 5) && (rhoLR[1][5] == 6);
+  passing = passing && (rhoLR[0][6] == 6) && (rhoLR[1][6] == 7);
+  passing = passing && (rhoLR[0][7] == 7) && (rhoLR[1][7] == 8);
+  passing = passing && (rhoLR[0][8] == 8) && (rhoLR[1][8] == 9);
   print_success(passing);
   if(!passing){
     print_array<nx-1,double>(rhoLR[0]);
@@ -95,14 +95,15 @@ int main(){
   passing = true;
   EOS eos;
   eos.gamma = 2.;
-  passing = passing & (eos.internal_energy_density(2.,3.) == 3./2.);
-  passing = passing & (eos.pressure(2.,3.) == 6.);
-  passing = passing & (eos.soundspeed(2.,3.)==sqrt(3.));
+  passing = passing && (eos.internal_energy_density(2.,3.) == 3./2.);
+  passing = passing && (eos.pressure(2.,3.) == 6.);
+  passing = passing && (eos.soundspeed(2.,3.)==sqrt(3.));
   print_success(passing);
   
   //==========//
   // Prim2Con //
   //==========//
+  
   cout << "Prim2Con: " << endl;
   
   //==========//
