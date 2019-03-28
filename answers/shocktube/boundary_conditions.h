2af1374f75ca63e85714e12824845b4ce43d9992
Sherwood Richers <srichers@caltech.edu>
2019-03-28 17:01:35 -0400

    comments

--- answers/shocktube/boundary_conditions.h
+++ answers/shocktube/boundary_conditions.h
@@ -3,11 +3,12 @@
 
 #include <array>
 
+// apply reflecting boundary conditions
+// ghost zones are literally a reflection of the non-ghost zones
+// call with:
+//     reflecting_boundary_conditions<nx,nghost>(conservative);
 template<int nx, int nghost>
-  void reflecting_boundary_conditions(array<array<double,nx>,3>& inout){
-  assert(nx > 2*nghost);
-
-  // set reflecting boundary conditions
+void reflecting_boundary_conditions(array<array<double,nx>,3>& inout){
   for(int v=0; v<3; v++){
     for(int i=0; i<nghost; i++){
       inout[v][i            ] = inout[v][2*nghost-1-i];
