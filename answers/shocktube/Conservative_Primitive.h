cfbbb3c53e3d2101fd55fd8598d436b4df60b734
Sherwood Richers <srichers@caltech.edu>
2019-03-28 17:04:06 -0400

    comments

--- answers/shocktube/Conservative_Primitive.h
+++ answers/shocktube/Conservative_Primitive.h
@@ -1,6 +1,11 @@
+#ifndef CON2PRIM_H
+#define CON2PRIM_H
+
 #include <array>
 
 // given conservative variables, return primitive variables
+// call with:
+//     primitive = get_primitive<nx>(conservative, eos);
 template<int nx>
 array<array<double,nx>,3> get_primitive(const array<array<double,nx>,3>& conservative,
 					const EOS& eos){
@@ -18,14 +23,15 @@ array<array<double,nx>,3> get_primitive(const array<array<double,nx>,3>& conserv
   array<double,nx> internal_energy_density;
   for(int i=0; i<nx; i++)
     internal_energy_density[i] = 1./conservative[0][i] *
-      (conservative[2][i] - .5*(conservative[1][i]*conservative[1][i]/conservative[0][i]) );
-    
+      (conservative[2][i] - .5*(conservative[1][i]*conservative[1][i]/conservative[0][i]) );    
   primitive[2] = eos.pressure<nx>(conservative[0], internal_energy_density);
     
   return primitive;
 }
 
 // given primitive variables, return conservative variables
+// call with:
+//     conservative = get_conservative<nx>(primitive, eos);
 template<int nx>
 array<array<double,nx>,3> get_conservative(const array<array<double,nx>,3>& primitive,
 					   const EOS& eos){
@@ -40,10 +46,14 @@ array<array<double,nx>,3> get_conservative(const array<array<double,nx>,3>& prim
     conservative[1][i] = primitive[0][i]*primitive[1][i];
 
   // total energy density
-  array<double,nx> internal_energy_density = eos.internal_energy_density<nx>(primitive[0],primitive[2]);
-
+  array<double,nx> internal_energy_density =
+    eos.internal_energy_density<nx>(primitive[0],primitive[2]);
   for(int i=0; i<nx; i++)
-    conservative[2][i] = primitive[0][i] * (.5*primitive[1][i]*primitive[1][i] + internal_energy_density[i]);
+    conservative[2][i] = primitive[0][i] *
+      (.5*primitive[1][i]*primitive[1][i] + internal_energy_density[i]);
 
   return conservative;
 }
+
+
+#endif
