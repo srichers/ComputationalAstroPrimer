8851e47dd9d10b1418e9f3d0faa74fc685794472
Sherwood Richers <srichers@berkeley.edu>
2022-08-02 18:56:19 -0700

    make naieve shock problem run (and fail) - had not implemented boundary conditions properly leading to premature nans

--- answers/naieve_shocktube/shocktube.cpp
+++ answers/naieve_shocktube/shocktube.cpp
@@ -8,12 +8,27 @@
 #include "misc.h"
 using namespace std;
 
-const int nx = 50;
+const int nx = 1000;
 const int nghost = 1;
 const double dx = 1./nx;
-const double tend = .0786603;
+const double tend = 0.25;
 const double courant = 0.1;
-const int arr_len = nx * 2*nghost;
+const int arrlen = nx + 2*nghost;
+
+void reflecting_boundary_conditions(array<double,arrlen>& px, array<double,arrlen>& rho, array<double,arrlen>& Eint){
+    for(int i=0; i<nghost; i++){
+      int iL_ghost = i;
+      int iL_real = 2*nghost-1-i;
+      int iR_ghost = arrlen-1-i;
+      int iR_real = arrlen-2*nghost+i;
+      px[iL_ghost] = px[iL_real];
+      px[iR_ghost] = px[iR_real];
+      rho[iL_ghost] = rho[iL_real];
+      rho[iR_ghost] = rho[iR_real];
+      Eint[iL_ghost] = Eint[iL_real];
+      Eint[iR_ghost] = Eint[iR_real];
+    }
+}
 
 int main(){
   // evolved variables. Assume tube cross-section of 1
@@ -34,13 +49,14 @@ int main(){
     px[i] = 0;
   }
   Eint = eos.Eint(rho,P);
+  reflecting_boundary_conditions(px, rho, Eint);
 
   // set up file
   int it=0;
   double t=0;
   ofstream output;
   output.open("output.dat");
-  output << "# it t i x rho Eint px" << endl;
+  output << "# it t dt i x rho Eint px" << endl;
   print(output, it, t, 0, x, rho, Eint, px);
   
   // start time integration
@@ -68,6 +84,9 @@ int main(){
     rho  = rho  +  drho_dt*dt;
     Eint = Eint + dEint_dt*dt;
 
+    // reflecting boundary conditions
+    reflecting_boundary_conditions(px, rho, Eint);
+    
     // output the fluid data
     print(output, it, t, dt, x, rho, Eint, px);
   }
