b83cbb3bc3b5e5bdcbcf548a088e60dc3c687e85
Sherwood Richers <srichers@berkeley.edu>
2022-10-30 13:04:00 -0700

    updated comments in TOV exercise

--- TOV/unidades.py
+++
@@ -1,8 +0,0 @@
-# constant values (cgs)
-# COMPLETA ESTO #
-#c =  # velocidad de luz, cm/s
-#h = # constante de Planck, erg*s
-#G = # constante de gravitacion, cm^3/g/s
-#mu0 = # masa del baryon, g
-#Msol = # masa del sol, g
-#km = # cm
